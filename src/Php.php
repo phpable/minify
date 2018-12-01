3796b09ff4a998325b26de77d691630585c1a4af
hacpaka <hacpaka@eggbe.com>
2018-12-01 11:12:51 +0300

    refactoring

--- src/Php.php
+++ src/Php.php
@@ -17,22 +17,21 @@ class Php extends AMinifier {
 		$Vars = [];
 		foreach ($Tokens as $i => $Info){
 			if (is_array($Info)){
+				echo trim($Info[1]) . ' ' . token_name($Info[0]) . "\n";
+
 				if (in_array($Info[0], [T_DOC_COMMENT, T_COMMENT])){
 					continue;
+				}
+
+				if ($Info[0] == T_STRING && $i > 0 && is_array($Tokens[$i - 1])
+					&& $Tokens[$i - 1][0] == T_OBJECT_OPERATOR){
+						$Info[1] = $this->replace($Info[1], $Vars);
 				}
 
 				if ($Info[0] == T_VARIABLE && !in_array($Info[1], ['$this', '$_POST', '$_GET', '$_REQUEST',
 					'$_FILES', '$_SERVER', '$_SESSION', '$_COOKIE', '$_ENV'])){
 
-					if (!isset($Vars[$Info[1]])){
-						$Vars[$Info[1]] = '$' . chr(97 + (count($Vars) % 26));
-
-						if (count($Vars) > 26) {
-							$Vars[$Info[1]] .= floor(count($Vars) / 26);
-						}
-					}
-
-					$Info[1] = $Vars[$Info[1]];
+					$Info[1] = '$' . $this->replace(substr($Info[1], 1), $Vars);
 				}
 
 				if ($Info[0] == T_WHITESPACE){
@@ -54,6 +53,23 @@ class Php extends AMinifier {
 		}
 
 		return $out;
+	}
+
+	/**
+	 * @param string $name
+	 * @param array $Vars
+	 * @return string
+	 */
+	private function replace(string $name, array &$Vars = []): string {
+		if (!isset($Vars[$name])){
+			$Vars[$name] = chr(97 + (count($Vars) % 26));
+
+			if (count($Vars) > 26) {
+				$Vars[$name] .= floor(count($Vars) / 26);
+			}
+		}
+
+		return $Vars[$name];
 	}
 
 }
